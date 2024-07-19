82c4c44ace0331f6b58497b6591802238d236b59
David Drah Llensa <llensa@fktech.net>
2024-07-19 15:50:43 -0300

    test

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -8,7 +8,7 @@ import Pricing from "./components/Pricing";
 export default () => {
   return (
     <div>
-      hola
+      holas
       <StylesProvider>
         <BrowserRouter>
           <Switch>
