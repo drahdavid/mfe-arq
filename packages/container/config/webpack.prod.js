6bd3735ded6cbc54d8262cda1a6ded0317a1377c
David Drah Llensa <llensa@fktech.net>
2024-07-19 11:45:42 -0300

    Modified config.

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -2,22 +2,24 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
-const productionConfig = {
+const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, productionConfig);
+module.exports = merge(commonConfig, prodConfig);
