0b8e9316f0b79705b9727526f666269c0a00bee0
David Drah Llensa <llensa@fktech.net>
2024-07-19 15:43:17 -0300

    Test.

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 export default () => {
   return (
     <div>
+      hola
       <StylesProvider>
         <BrowserRouter>
           <Switch>
