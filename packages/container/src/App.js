34c345c1c09fe37c087e3531dc3f80cd7e9c6178
David Drah Llensa <llensa@fktech.net>
2024-07-22 09:53:21 -0300

    Changes prefix classname logic for material ui to avoid name collision.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "container",
+});
 
 export default () => {
   return (
     <div>
       <BrowserRouter>
-        <Header></Header>
-        <MarketingApp />
+        <StylesProvider generateClassName={generateClassName}>
+          <Header />
+          <MarketingApp />
+        </StylesProvider>
       </BrowserRouter>
     </div>
   );
